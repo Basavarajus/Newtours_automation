ef04fd63e54404e9603acf9cb84267831b0506d2
basavaraju.s <basavaraju.s@simplilearn.net>
2020-05-02 13:05:02 +0530

    added testcases to flightbookTest class

--- src/main/java/com/newtours/demoaut/pages/FlightBookPage.java
+++ src/main/java/com/newtours/demoaut/pages/FlightBookPage.java
@@ -59,14 +59,29 @@ public class FlightBookPage extends TestBase {
 	@FindBy(xpath="//input[@name='reserveFlights']")
 	WebElement confirm_booking;
 	
+	@FindBy(xpath="//select[@name='pass.0.meal']")
+	WebElement Meal_type;
+	
+	@FindBy(xpath="//input[@name='passFirst0']")
+	WebElement first_name;
+	
+	@FindBy(xpath="//input[@name='passLast0']")
+	WebElement lastname;
+	
+	@FindBy(xpath="//input[@name='creditnumber']")
+	WebElement card_number;
+	
+	@FindBy(xpath="//input[@src='/images/forms/purchase.gif']")
+	WebElement secure_purchase;
 	
 	
 	public FlightBookPage() {
 		PageFactory.initElements(driver, this);
 	}
 	
-	public void verifyFlightbookProcess(String source,String Destination ,String airline) {
-		flightFindertextlabel.isDisplayed();
+	public void verifyFlightbookProcess(String source,String Destination ,String airline,String Fname,String Lname,String Meal,String Number) {
+//		flightFindertextlabel.isDisplayed();
+		System.out.println(source+"------ "+Destination+"-----"+airline);
 		Select FromPlace= new Select(From_place);
 		FromPlace.selectByVisibleText(source);
 		Select Toplace= new Select(To_place);
@@ -75,6 +90,14 @@ public class FlightBookPage extends TestBase {
 		airlines.selectByVisibleText(airline);
 		continue_link.click();
 		confirm_booking.click();
+		first_name.sendKeys(Fname);
+		lastname.sendKeys(Lname);
+		Select Meal_types= new Select(Meal_type);
+		Meal_types.selectByVisibleText(Meal);
+		card_number.sendKeys(Number);
+		
+		secure_purchase.click();
+		
 		
 	}
 	
--- src/test/java/com/newtours/demoaut/tests/FlightBookPageTest.java
+++ src/test/java/com/newtours/demoaut/tests/FlightBookPageTest.java
@@ -10,6 +10,7 @@ import com.newtours.demoaut.base.TestBase;
 import com.newtours.demoaut.pages.FlightBookPage;
 import com.newtours.demoaut.pages.HomePage;
 import com.newtours.demoaut.pages.LoginPage;
+import com.newtours.demoaut.pages.RegistrationPage;
 import com.newtours.demoaut.utility.Utility;
 
 public class FlightBookPageTest extends TestBase {
@@ -17,21 +18,33 @@ public class FlightBookPageTest extends TestBase {
 	HomePage home;
 	Utility util;
 	LoginPage loginpage;
+	RegistrationPage registration;
 	FlightBookPage flightbook;
 	String Sheet="Registration";
 
+	
+	public FlightBookPageTest() {
+		super();
+	}
 	@BeforeMethod
-	public void setup() {
+	public void setup() throws InterruptedException {
 		initialization();
+		registration= new RegistrationPage();
+		loginpage= new LoginPage();
+//		loginpage=registration.Registeruser(util.getDataFromXL(Sheet, 1, 0),(util.getDataFromXL(Sheet, 1, 1)),(util.getDataFromXL(Sheet, 1, 3)),(util.getDataFromXL(Sheet, 1, 10)),(util.getDataFromXL(Sheet, 1, 8)));
 		home=loginpage.VerifyLoginFunctionality((util.getDataFromXL(Sheet, 1, 3)),(util.getDataFromXL(Sheet, 1, 10)));
 //		home = new HomePage();
 		flightbook = home.VerifyFlightBookPagelink();
 	}
 	
+	@SuppressWarnings("static-access")
 	@Test(priority=1)
 	public void verifyflightBooking() {
-		flightbook.verifyFlightbookProcess((util.getDataFromXL(Sheet, 7, 1)),(util.getDataFromXL(Sheet, 7, 2)),(util.getDataFromXL(Sheet, 7, 4)));
+		flightbook.verifyFlightbookProcess((util.getDataFromXL(Sheet, 5, 0)),(util.getDataFromXL(Sheet, 5, 1)),(util.getDataFromXL(Sheet, 5, 3)),(util.getDataFromXL(Sheet, 1, 0)),(util.getDataFromXL(Sheet, 1, 1)),(util.getDataFromXL(Sheet, 5, 4)),(util.getDataFromXL(Sheet, 5, 5)));
+		System.out.println((util.getDataFromXL(Sheet, 5, 0))+"-----"+(util.getDataFromXL(Sheet, 5, 2))+"----"+(util.getDataFromXL(Sheet, 5, 3)));
 	}
+	
+	
 	
 	@AfterMethod
 	public void teardown() {
